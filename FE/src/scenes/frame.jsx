29f72c7351dfe4387e30aba779d65c9afd97f71e
mazksr <azkasufirman3@gmail.com>
2025-01-21 16:15:30 +0800

    add build

--- FE/src/scenes/frame.jsx
+++ FE/src/scenes/frame.jsx
@@ -2,7 +2,7 @@ import { Box, Button, Grid, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { HeaderData } from "../data/headerCostum";
 import { useAuth } from "../auth/auth_provider";
-import Logo from '../assets/image/makassar.png';
+import Logo from '../assets/image/makassar-white.png';
 import PatternImage from '../assets/image/patternbg.png'
 import "../tailwind.css"
 
@@ -11,55 +11,49 @@ const Frame = () => {
 
     return (
         <Box sx={{
+            paddingTop: 4,
             minHeight: '100vh',
             background: 'linear-gradient(to top right, #000, #8E1616)',
             paddingBottom: 4,
             position: 'relative',
+            zIndex: 0,
             '&::before': {
                 content: '""',
                 position: 'absolute',
                 bottom: 0,
                 left: 0,
                 right: 0,
-                height: '100%', 
-                backgroundImage: `url(${PatternImage})`, 
-                backgroundSize: 'cover', 
-                backgroundRepeat: 'repeat', 
-                opacity: 0.3, 
+                height: '100%',
+                backgroundImage: `url(${PatternImage})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'repeat',
+                opacity: 0.3,
                 pointerEvents: 'none',
-                zIndex: 0
+                zIndex: -1
             }
         }}>
-            <Grid container sx={{
-                padding: 2,
-                position: 'relative',
-                zIndex: 1,
-                display: "flex",
-                alignItems: 'center' 
-            }}>
+            <div className="flex justify-center items-end mb-4">
                 <Grid item>
                     <img src={Logo} alt="icon" style={{ width: 180, height: 110, marginRight: 10 }} />
                 </Grid>
                 <Grid item>
                     <Typography
-                    fontFamily={'sans-serif'}
-                    fontWeight={700}
-                    fontSize={30}
-                    align="left"
-                    textTransform={"uppercase"}
-                    sx={{
-                        color: '#EA3525',
-                        textShadow: '2px 2px 2px rgba(0,0,0,0.1)'
-                    }}
+                        className={"drop-shadow-[0_1.2px_1.2px_rgba(255,255,255,5)] text-[#EA3525] mb-2"}
+                        fontFamily={'sans-serif'}
+                        fontWeight={700}
+                        fontSize={48}
+                        align="left"
+                        textTransform={"uppercase"}
                     >
-                    {"Makassar Virtual Economic Center"}
+                        {"Makassar Virtual Economic Center"}
                     </Typography>
                 </Grid>
-            </Grid>
+            </div>
 
-            <iframe src={"localhost:3000"} className={"absolute top-1/2 left-1/2 w-[85%] h-[73%] transform -translate-x-1/2 -translate-y-1/2 text-white p-4 rounded"}>
-            </iframe>
-
+            <iframe
+                src={"https://makassarv2.sakti112.id/login"}
+                className="w-[80%] h-[80vh] mx-auto rounded-lg"
+            />
 
         </Box>
     );
